513b684ed133f30a907f2b288055f04342b72037
Alex Marques <am109@myscc.ca>
2026-04-03 16:14:54 -0400

    Finished school projects for now

--- src/app/app.ts
+++ src/app/app.ts
@@ -30,19 +30,49 @@ export class App {
       description: 'A personal portfolio built using Angular, Show cases projects, skills, and contact information in a responsive layout.',
       tags: ['Angular', 'TypeScript', 'CSS'],
 
-    }
+    },
     
     {
       title: 'Resume',
       course: 'MIT207',
       description: 'A resume new nad improved resume built with Microsoft Word to allow our work to pop.',
-      tags: ['MicrosftWord']
-    }
+      tags: ['MicrosftWord'],
+    },
 
     {
       title: 'PowerPoint Presentation',
       course: 'MIT207',
-      description: 'A PowerPoint Presentation'
+      description: 'A PowerPoint Presentation built to teach the students how to build a Presentation for real world scenarios.',
+      tags: ['MicrosoftPowerPoint'],
+
+    },
+
+    {
+      title: 'Elevator Pitch',
+      course: 'MIT207',
+      description: 'A Task that was designed to teach us the importance of knowing who you are and how to convey it alongside your value within 30 seconds.',
+      tags: ['Communication'],
+    },
+
+    {
+      title: 'Mock Interview',
+      course: 'MIT207',
+      description: 'A assignment designed to simulate attending a real world interview preparing us for when we truly have a interview for a position.',
+      tags: ['Communication', 'Punctuality'],
+    },
+
+    {
+      title: 'Group Project',
+      course: 'MIT207',
+      description: 'A task completed in a small group to simulate the real-world working environment',
+      tags: ['Communication', 'Collaboration'],
+    },
+
+    {
+      title: 'Verbal Presentation',
+      course: 'MIT207',
+      description: 'A task done as individuals to improve ones ability to perform a presentation to peers or management in the workplace.',
+      tags: ['Communication'],
     }
   ]
 
@@ -53,7 +83,7 @@ export class App {
       period: 'Nov 2018 - Jun 2022',
       description: 'Used to Zebra TC5X Series Mobile Computer to keep track of inventory, and stack shelves. Also baked fresh products and kept the work area hygenic.',
 
-    }
+    },
 
     {
       role: 'General Laborer',
@@ -61,7 +91,7 @@ export class App {
       period: 'Sept 2022 - Mar 2023',
       description: 'Used basic tools and machinery, assisted in the removal, and replacing of roofing systems. Also assisted in fixing or patching roofing systems.',
 
-    }
+    },
 
     {
       role: 'General Laborer',
@@ -69,7 +99,7 @@ export class App {
       period: 'Mar 2023 - Jul 2023',
       description: 'Learned plumbing specific tools and assisted the journeyman plumber in their different types of projects each day.',
 
-    }
+    },
 
     {
       role: 'Forklift Driver',
@@ -77,7 +107,7 @@ export class App {
       period: 'Jan 2024 - Dec 2024',
       description: 'Loaded and offloaded product on to trucks. Kept inventory and the workplace organized.',
 
-    }
+    },
 
     {
       role: 'Warehouse Supervisor',
@@ -85,6 +115,6 @@ export class App {
       period: 'Dec 2024 - Sept 2025',
       description: 'Lead my team through production, ensured the tasks of the day were completed, and ensured all robots and machinery were running efficently.',
 
-    }
+    },
   ]
 }
